deada9e6fdf2fbca2fa74f81e6f3b7a9b6d3f3b5
prosdl <rosendahl@prosdl.de>
2015-02-18 23:06:40 +0100

    HibernateConfig

--- mfthub-model/src/test/java/de/mfthub/model/SimpleTestConfiguration.java
+++ mfthub-model/src/test/java/de/mfthub/model/SimpleTestConfiguration.java
@@ -3,9 +3,12 @@ package de.mfthub.model;
 import javax.sql.DataSource;
 
 import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.orm.hibernate4.HibernateTransactionManager;
@@ -16,7 +19,14 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @Configuration
 @EnableTransactionManagement
 @ComponentScan("de.mfthub")
+@PropertySource("classpath:hibernate-test.properties")
 public class SimpleTestConfiguration {
+
+   @Value("${hibernate.dialect}")
+   private String hibernateDialect;
+
+   @Value("${hibernate.hbm2ddl.auto}")
+   private String hibernateHbm2ddlAuto;
 
    @Bean
    public DataSource dataSource() {
@@ -25,11 +35,16 @@ public class SimpleTestConfiguration {
    }
 
    @Bean
+   public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+      return new PropertySourcesPlaceholderConfigurer();
+   }
+
+   @Bean
    public SessionFactory sessionFactory() {
       return new LocalSessionFactoryBuilder(dataSource())
             .scanPackages("de.mfthub.model.entities")
-            .setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect")
-            .setProperty("hibernate.hbm2ddl.auto", "update")
+            .setProperty("hibernate.dialect", hibernateDialect)
+            .setProperty("hibernate.hbm2ddl.auto", hibernateHbm2ddlAuto)
             .setProperty("hibernate.show_sql", "true")
             .setProperty("hibernate.format_sql", "true").buildSessionFactory();
    }
