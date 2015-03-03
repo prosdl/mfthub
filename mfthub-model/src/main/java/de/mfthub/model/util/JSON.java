65a2f477082869a91947315fb967613905d6a0a4
prosdl <rosendahl@prosdl.de>
2015-03-03 20:17:39 +0100

    Changed date format for json output.

--- mfthub-model/src/main/java/de/mfthub/model/util/JSON.java
+++ mfthub-model/src/main/java/de/mfthub/model/util/JSON.java
@@ -3,15 +3,24 @@ package de.mfthub.model.util;
 import java.io.IOException;
 
 import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.SerializationConfig;
 import org.codehaus.jackson.map.annotate.JsonSerialize.Inclusion;
 
 public class JSON {
    
-   public static String toJson(Object o) {
-      ObjectMapper om = new ObjectMapper();
+   // ObjectMappers are thread safe, see: jackson faq
+   private final static ObjectMapper JACKSON_JSON_OBJECT_MAPPER = initObjectMapper();
+   
+   private static ObjectMapper initObjectMapper() {
+      ObjectMapper om =  new ObjectMapper();
       om.setSerializationInclusion(Inclusion.NON_NULL);
+      om.configure(SerializationConfig.Feature.WRITE_DATES_AS_TIMESTAMPS, false);
+      return om;
+   }
+   
+   public static String toJson(Object o) {
       try {
-         return om.writerWithDefaultPrettyPrinter().writeValueAsString(o);
+         return JACKSON_JSON_OBJECT_MAPPER.writerWithDefaultPrettyPrinter().writeValueAsString(o);
       } catch (IOException e) {
          throw new RuntimeException(e);
       }
